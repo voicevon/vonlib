7c074bb96612cb2622c4c7e83db5f5e50b0a2b2c
Xuming <voicevon@gmail.com>
2023-06-17 15:14:48 +0800

    led spinonce()

--- src/von/component/display/mono_led_gpio.cpp
+++ src/von/component/display/mono_led_gpio.cpp
@@ -1,9 +1,34 @@
 #include "mono_led_gpio.h"
-
+#include "von/utility/logger.h"
 // Led::Led(uint8_t id, uint8_t pin, uint8_t turn_on_level):BinaryOutput_GPIO(id, pin, turn_on_level){
     
 // }
 
-void Mono_Led_GPIO::Flash(){
-    
+
+void Mono_Led_GPIO::SetState(EnumState new_state){
+    switch (__state){
+        case TURN_ON:
+            this->TurnOn();
+            break;
+        case TURN_OFF:
+            this->TurnOff();
+            break;
+        case FLASH_1Hz:
+            break;
+        case FLASH_2Hz:
+            break;
+        default:
+            Logger::Error("Mono_Led_GPIO::SetState()  Unknown new_state");
+            Logger::Print("new_state", new_state);
+            Logger::Halt("");
+    }
+}
+
+void Mono_Led_GPIO::SpinOnce(){
+    switch (__state){
+        case FLASH_1Hz:
+            break;
+        default:
+            break;
+    }
 }
--- src/von/component/display/mono_led_gpio.h
+++ src/von/component/display/mono_led_gpio.h
@@ -7,12 +7,21 @@
 class Mono_Led_GPIO: public BinaryOutput_GPIO
 {
     public:
+        enum EnumState{
+            TURN_ON = 1,
+            TURN_OFF = 2 ,
+            FLASH_1Hz = 101,
+            FLASH_2Hz = 102,
+        };
         Mono_Led_GPIO():BinaryOutput_GPIO(){};   // Is this necessary?
         Mono_Led_GPIO(uint8_t id, uint8_t pin, uint8_t turn_on_level):BinaryOutput_GPIO(id, pin, turn_on_level){};
-        void Flash();
+        // void Flash();
+        void SpinOnce();
+        void SetState(EnumState new_state);
+
     protected:
 
     private:
-
+        EnumState __state;
 };
 
